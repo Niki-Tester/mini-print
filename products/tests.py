aac3a736dcf148f8f42fa4d054ffac9ae273cf88
Niki Tester <bloatedniknak@gmail.com>
2022-07-19 10:58:55 +0000

    add product / products detail tests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    """
+    Test cases for products app views
+    """
+
+    def test_products_page(self):
+        """ Test products page loads """
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_products_details_page(self):
+        """ Test product details page loads """
+        response = self.client.get('/products/2/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/product_detail.html')
